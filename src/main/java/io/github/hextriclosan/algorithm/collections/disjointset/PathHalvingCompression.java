7679f3f304ad69b4527f78aba044219b79fd9a43
Igor Rudenko <iostream64@gmail.com>
2025-11-01 00:32:38 +0200

    Use java version 17

--- src/main/java/io/github/hextriclosan/algorithm/collections/disjointset/PathHalvingCompression.java
+++ src/main/java/io/github/hextriclosan/algorithm/collections/disjointset/PathHalvingCompression.java
@@ -9,13 +9,18 @@ import java.util.Map;
  * @param <E> the type of elements in the Disjoint Set
  */
 public class PathHalvingCompression<E> implements FindCompressStrategy<E> {
+    /**
+     * Creates a new PathHalvingCompression strategy.
+     */
+    public PathHalvingCompression() {
+    }
 
     /**
      * Applies the path halving compression strategy to find the root of the subset containing the specified element.
      * If the element is not found in the map, an {@link IllegalArgumentException} is thrown.
      *
      * @param parentByElement the map representing the parent relationship of elements in the disjoint set
-     * @param element the element for which to find the root
+     * @param element         the element for which to find the root
      * @return the root of the subset containing the specified element
      * @throws IllegalArgumentException if the element is not found in the disjoint set
      */
