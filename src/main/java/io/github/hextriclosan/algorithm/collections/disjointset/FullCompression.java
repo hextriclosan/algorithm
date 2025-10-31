ef81d9bbb4ef08db284d3d05d2899f2239e236db
Igor Rudenko <iostream64@gmail.com>
2025-11-01 00:32:38 +0200

    Use java version 17

--- src/main/java/io/github/hextriclosan/algorithm/collections/disjointset/FullCompression.java
+++ src/main/java/io/github/hextriclosan/algorithm/collections/disjointset/FullCompression.java
@@ -10,13 +10,18 @@ import java.util.Map;
  * @param <E> the type of elements in the Disjoint Set
  */
 public class FullCompression<E> implements FindCompressStrategy<E> {
+    /**
+     * Creates a new FullCompression strategy.
+     */
+    public FullCompression() {
+    }
 
     /**
      * Applies the full path compression strategy to find the root of the subset containing the specified element.
      * If the element is not found in the map, an {@link IllegalArgumentException} is thrown.
      *
      * @param parentByElement the map representing the parent relationship of elements in the disjoint set
-     * @param element the element for which to find the root
+     * @param element         the element for which to find the root
      * @return the root of the subset containing the specified element
      * @throws IllegalArgumentException if the element is not found in the disjoint set
      */
